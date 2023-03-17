21cdc6cb6d79611f1243ae5ed6b0625bbbb65792
nik-markovic <nikola.markovic@avnet.com>
2023-03-17 11:53:25 -0700

    Fix for github actions

--- scripts/setup-project.sh
+++ scripts/setup-project.sh
@@ -2,6 +2,16 @@
 
 set -e # fail on errors
 
-cd $(dirname "$0")/../
+cd $(dirname "$0")
 
-git archive HEAD --prefix=iotc-mchp-avr-sdk/  --format=zip -o iotc-mchp-avr-sdk.zip
+if [[ "$1" == "github-action-upload" ]]; then
+  rm -rf /tmp/iotc-mchp-avr-sdk
+  mkdir -p /tmp/iotc-mchp-avr-sdk
+  cp -r * /tmp/iotc-mchp-avr-sdk
+  rm -rf /tmp/iotc-mchp-avr-sdk/scripts
+  mv /tmp/iotc-mchp-avr-sdk .
+
+else
+  cd ../
+  git archive HEAD --prefix=iotc-mchp-avr-sdk/  --format=zip -o iotc-mchp-avr-sdk.zip
+fi
