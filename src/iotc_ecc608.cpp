efdbc2d2ceaa10f3677875923a90cc2d61a96b9d
nik-markovic <nikola.markovic@avnet.com>
2023-05-23 14:25:02 -0500

    IoTConnect parameter provisioning bugfixes + unit test

--- src/iotc_ecc608.cpp
+++ src/iotc_ecc608.cpp
@@ -28,11 +28,6 @@ static ATCAIfaceCfg cfg_atecc608b_i2c = {
 #define IOTC_DATA_SLOT_SIZE 416 // This should be the full size of the data zone slot 8
 #endif
 
-#define IOTC_ECC608_PROV_CPID GOOGLE_PROJECT_ID
-#define IOTC_ECC608_PROV_ENV  GOOGLE_PROJECT_REGION
-#define IOTC_ECC608_PROV_VER  GOOGLE_REGISTRY_ID      // Data storage version (check)
-#define IOTC_ECC608_PROV_DUID GOOGLE_DEVICE_ID
-
 // How many bytes will each string have in reserved storaage.
 // Storage content may be arbitrary after the first null terminator.
 
@@ -67,32 +62,22 @@ static ATCA_STATUS init_ecc608(void) {
     return atca_status;
 }
 
-static ATCA_STATUS write_all_data(void) {
-    ATCA_STATUS atca_status;
-    if ((atca_status = atcab_write_bytes_zone(
-        ATCA_ZONE_DATA,
-        DATA_SLOT_NUM,
-        0,
-        data_cache,
-        IOTC_DATA_SLOT_SIZE)) != ATCA_SUCCESS
-    ) {
-        Log.errorf("Failed to write provisioning data! Error %d\r\n", atca_status);
-        return atca_status;
-    }
-}
 static size_t ecchdr_get_data_size(DataHeaderUnion* h) {
     uint16_t this_header_offset = (char*) h - data_cache;
     return (h->header.next - this_header_offset - sizeof(DataHeaderUnion));
 }
+
 static DataHeaderUnion* ecchdr_next(DataHeaderUnion* h) {
     if (!h) {
         return (DataHeaderUnion*) data_cache;
     }
     return (DataHeaderUnion*) &data_cache[h->header.next];
 }
+
 static char* ecchdr_data_ptr(DataHeaderUnion* h) {
     return ((char*)h) + sizeof(DataHeaderUnion);
 }
+
 static DataHeaderUnion* append_iotconnect_blank_record(DataHeaderUnion* h, uint16_t type,  size_t data_size) {
     uint16_t this_header_offset = (char*) h - data_cache;
     h->header.next =  this_header_offset + sizeof(DataHeaderUnion) + (uint16_t) data_size;
@@ -111,6 +96,62 @@ static void append_iotconnect_blank_records(DataHeaderUnion* start) {
     start->header.next = 0;
 }
 
+static ATCA_STATUS iotc_ecc608_get_string_value_internal(ecc_data_types data_type, char ** value) {
+    DataHeaderUnion* h = (DataHeaderUnion *) data_cache;
+    *value = NULL;
+    switch (data_type) {
+        case IOTC_ECC608_PROV_VER:
+        case IOTC_ECC608_PROV_ENV:
+        case IOTC_ECC608_PROV_CPID:
+        case IOTC_ECC608_PROV_DUID:
+            while(h->header.type != EMPTY) {
+                if (h->header.type == data_type) {
+                    *value = ecchdr_data_ptr(h);
+                    return ATCA_SUCCESS;
+                }
+                h = ecchdr_next(h);
+            }
+            return ATCA_INVALID_ID;
+
+        default:
+            Log.error("IOTC_ECC608: Only IOTCONNECT data types are supported. Use copy_string_value instead.");
+            return ATCA_BAD_PARAM;
+    }
+}
+
+static ATCA_STATUS iotc_ecc608_set_string_value_internal(ecc_data_types data_type, const char * value) {
+    DataHeaderUnion* h = (DataHeaderUnion *) data_cache;
+    while(h->header.type != EMPTY) {
+        if (h->header.type == data_type) {
+            size_t size = ecchdr_get_data_size(h);
+            switch (data_type) {
+                case IOTC_ECC608_PROV_VER:
+                case IOTC_ECC608_PROV_ENV:
+                case IOTC_ECC608_PROV_CPID:
+                case IOTC_ECC608_PROV_DUID:
+                    // iotconnect data re null terminated strings
+                    if ((strlen(value) + 1) > size) {
+                        Log.error("IOTC_ECC608: String size is larger than reserved size!");
+                        return ATCA_INVALID_LENGTH;
+                    }
+                    strcpy(ecchdr_data_ptr(h), value);
+                    break;
+                default:
+                    if ((strlen(value)) > size) {
+                        Log.error("IOTC_ECC608: String size is larger than reserved size!");
+                        return ATCA_INVALID_LENGTH;
+                    }
+                    memcpy(ecchdr_data_ptr(h), value, size);
+                    break;
+            }
+            return ATCA_SUCCESS;
+        }
+        h = ecchdr_next(h);
+    }
+    Log.error("IOTC_ECC608: Data type %d was not found in storage. Unable to write value.!");
+    return ATCA_INVALID_ID;
+}
+
 static ATCA_STATUS load_ecc608_cache(void) {
     ATCA_STATUS atca_status;
     size_t slot_size;
@@ -125,7 +166,9 @@ static ATCA_STATUS load_ecc608_cache(void) {
         return atca_status;
     }
 
-    //Log.infof("Slot size: %lu\r\n", (unsigned long)slot_size);
+    if (slot_size != IOTC_DATA_SLOT_SIZE) {
+        Log.error("IOTC_ECC608: Unexpected data slot size received from ECC608!"); // Unlikely, so just a safeguard
+    }
 
     atca_status = atcab_read_bytes_zone(
         ATCA_ZONE_DATA,
@@ -137,10 +180,6 @@ static ATCA_STATUS load_ecc608_cache(void) {
     if (ATCA_SUCCESS != atca_status) {
         Log.error("IOTC_ECC608: Failed to read provisioning info!");
         return atca_status;
-    }
-
-    if (slot_size != IOTC_DATA_SLOT_SIZE) {
-        Log.error("IOTC_ECC608: Unexpected data slot size received from ECC608!"); // Unlikely, so just a safeguard
     }
 
     bool has_iotconnect_data = false;
@@ -161,12 +200,11 @@ static ATCA_STATUS load_ecc608_cache(void) {
     }
     if (!has_iotconnect_data) {
         append_iotconnect_blank_records(h);
-        atca_status = iotc_ecc608_write_string_value(IOTC_ECC608_PROV_VER, IOTC_ECC608_PROV_DATA_VERSION);
+        atca_status = iotc_ecc608_set_string_value_internal(IOTC_ECC608_PROV_VER, IOTC_ECC608_PROV_DATA_VERSION);
 
     }
     return ATCA_INVALID_ID;
 }
-
 
 // for debugging purposes
 void iotc_ecc608_dump_provision_data(void) {
@@ -205,31 +243,6 @@ ATCA_STATUS iotc_ecc608_init_provision(void) {
     return ATCA_SUCCESS;
 }
 
-
-static ATCA_STATUS iotc_ecc608_get_string_value_internal(ecc_data_types data_type, char ** value) {
-    DataHeaderUnion* h = (DataHeaderUnion *) data_cache;
-    *value = NULL;
-    switch (data_type) {
-        case IOTC_ECC608_PROV_VER:
-        case IOTC_ECC608_PROV_ENV:
-        case IOTC_ECC608_PROV_CPID:
-        case IOTC_ECC608_PROV_DUID:
-            while(h->header.type != EMPTY) {
-                if (h->header.type == data_type) {
-                    *value = ecchdr_data_ptr(h);
-                    return ATCA_SUCCESS;
-                }
-                h = ecchdr_next(h);
-            }
-            return ATCA_INVALID_ID;
-
-        default:
-                Log.error("IOTC_ECC608: Only IOTCONNECT data types are supported.Use copy_string_value instead.");
-            return ATCA_BAD_PARAM;
-    }
-
-}
-
 ATCA_STATUS iotc_ecc608_get_string_value(ecc_data_types data_type, char ** value) {
     if (data_type == IOTC_ECC608_PROV_VER) {
         *value = NULL;
@@ -239,13 +252,13 @@ ATCA_STATUS iotc_ecc608_get_string_value(ecc_data_types data_type, char ** value
 }
 
 // size is in/out: in=max / out=actual
-ATCA_STATUS iotc_ecc608_copy_string_value(ecc_data_types data_type, char *buffer, uint16_t buffer_size) {
+ATCA_STATUS iotc_ecc608_copy_string_value(ecc_data_types data_type, char *buffer, size_t buffer_size) {
     DataHeaderUnion* h = (DataHeaderUnion *) data_cache;
     buffer[0] = 0;
     while(h->header.type != EMPTY) {
         if (h->header.type == data_type) {
             switch (data_type) {
-                case IOTC_ECC608_PROV_VER:
+                case IOTC_ECC608_PROV_VER: // iotc_ecc608_get_string_value will warn about this data_type
                 case IOTC_ECC608_PROV_ENV:
                 case IOTC_ECC608_PROV_CPID:
                 case IOTC_ECC608_PROV_DUID:
@@ -279,34 +292,70 @@ ATCA_STATUS iotc_ecc608_copy_string_value(ecc_data_types data_type, char *buffer
     return ATCA_INVALID_ID;
 }
 
-ATCA_STATUS iotc_ecc608_write_string_value(ecc_data_types data_type, const char * value) {
-    DataHeaderUnion* h = (DataHeaderUnion *) data_cache;
-    while(h->header.type != EMPTY) {
-        if (h->header.type == data_type) {
-            size_t size = ecchdr_get_data_size(h);
-            switch (data_type) {
-                case IOTC_ECC608_PROV_VER:
-                case IOTC_ECC608_PROV_ENV:
-                case IOTC_ECC608_PROV_CPID:
-                case IOTC_ECC608_PROV_DUID:
-                    // iotconnect data re null terminated strings
-                    if ((strlen(value) + 1) > size) {
-                        Log.error("IOTC_ECC608: String size is larger than reserved size!");
-                        return ATCA_INVALID_LENGTH;
-                    }
-                    strcpy(ecchdr_data_ptr(h), value);
-                    break;
-                default:
-                    if ((strlen(value)) > size) {
-                        Log.error("IOTC_ECC608: String size is larger than reserved size!");
-                        return ATCA_INVALID_LENGTH;
-                    }
-                    memcpy(ecchdr_data_ptr(h), value, size);
-                    break;
-            }
-            return ATCA_SUCCESS;
-        }
-        h = ecchdr_next(h);
+ATCA_STATUS iotc_ecc608_set_string_value(ecc_data_types data_type, const char * value) {
+    if (data_type == IOTC_ECC608_PROV_VER) {
+        Log.error("IOTC_ECC608: Warning: User code should not be writing IOTC_ECC608_PROV_VER");
+        return ATCA_BAD_PARAM;
     }
-    return ATCA_INVALID_ID;
+    return iotc_ecc608_set_string_value_internal(data_type, value);
+}
+
+ATCA_STATUS iotc_ecc608_write_all_data(void) {
+    ATCA_STATUS atca_status;
+    atca_status = atcab_write_bytes_zone(
+        ATCA_ZONE_DATA,
+        DATA_SLOT_NUM,
+        0, // offset
+        (uint8_t*) data_cache,
+        IOTC_DATA_SLOT_SIZE // we already checked that the actual size matches
+    );
+    if (ATCA_SUCCESS != atca_status) {
+        Log.errorf("Failed to write provisioning data! Error %d\r\n", atca_status);
+        return atca_status;
+    }
+    return ATCA_SUCCESS;
+}
+
+// Keeping this function for reference only. It shows some usage examples.
+void iotc_ecc608_unit_test (void){
+    iotc_ecc608_init_provision();
+    iotc_ecc608_dump_provision_data();
+    #define BUFF_SIZE 130
+    #define LONG_STRING "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+    char buffer[BUFF_SIZE];
+    char* value;
+
+    strcpy(buffer, LONG_STRING);
+    if (iotc_ecc608_copy_string_value(AWS_THINGNAME, buffer, BUFF_SIZE)) { return; }
+    Log.infof("AWS_THINGNAME: %s\r\n", buffer);
+
+    Log.infof("Expected error: ");
+    if (0 == iotc_ecc608_copy_string_value(AWS_THINGNAME, buffer, 5)) { return; }
+
+    Log.infof("Expected warning: ");
+    if (iotc_ecc608_copy_string_value(IOTC_ECC608_PROV_VER, buffer, BUFF_SIZE)) { return; }
+    Log.infof("IOTC_ECC608_PROV_VER: %s\r\n", buffer);
+
+    Log.infof("Expected warning: ");
+    if (iotc_ecc608_get_string_value(IOTC_ECC608_PROV_VER, &value)) { return; }
+    Log.infof("IOTC_ECC608_PROV_VER: %s\r\n", value);
+
+    Log.infof("Expected error: ");
+    if (0 == iotc_ecc608_set_string_value(IOTC_ECC608_PROV_VER, "v2.0")) { return; }
+
+    Log.infof("Expected error: ");
+    if (0 == iotc_ecc608_set_string_value(IOTC_ECC608_PROV_CPID, "12345678901234567890123456789012345678901234567890123456789012345678901234567890")) { return; }
+    if (iotc_ecc608_set_string_value(IOTC_ECC608_PROV_CPID, "MY_CPID")) { return; }
+    strcpy(buffer, LONG_STRING);
+    if (iotc_ecc608_copy_string_value(IOTC_ECC608_PROV_CPID, buffer, BUFF_SIZE)) { return; }
+    Log.infof("IOTC_ECC608_PROV_CPID: %s\r\n", buffer);
+    if (iotc_ecc608_get_string_value(IOTC_ECC608_PROV_CPID, &value)) { return; }
+    Log.infof("IOTC_ECC608_PROV_CPID: %s\r\n", value);
+
+    Log.infof("Expected error: ");
+    if (0 == iotc_ecc608_set_string_value(AZURE_ID_SCOPE, "dummy")) { return; }
+
+    iotc_ecc608_dump_provision_data();
+
+    //iotc_ecc608_write_all_data();
 }
--- src/iotc_ecc608.h
+++ src/iotc_ecc608.h
@@ -1,3 +1,6 @@
+#ifndef IOTC_ECC608_H
+#define IOTC_ECC608_H
+
 //
 // Copyright: Avnet 2023
 // Created by Nik Markovic <nikola.markovic@avnet.com> on 5/16/21.
@@ -12,11 +15,19 @@
 #define IOTC_ECC608_PROV_DUID GOOGLE_DEVICE_ID
 
 ATCA_STATUS iotc_ecc608_init_provision(void);
+
 void iotc_ecc608_dump_provision_data(void);
 
 // Read data_type value into storage pointed by the "value"
-ATCA_STATUS iotc_ecc608_read_string_value(ecc_data_types data_type, char ** value);
-// Write the value pointed by "value" to ecc608 data storage
-ATCA_STATUS iotc_ecc608_write_string_value(ecc_data_types data_type, const char * value);
+ATCA_STATUS iotc_ecc608_get_string_value(ecc_data_types data_type, char ** value);
 
+// Read the value into "buffer".
+ATCA_STATUS iotc_ecc608_copy_string_value(ecc_data_types data_type, char *buffer, size_t buffer_size);
 
+// Write data to local cache, but to not write. Call commit() in order to write data into ecc608.
+ATCA_STATUS iotc_ecc608_set_string_value(ecc_data_types data_type, const char * value);
+
+// Write data to ecc608 Data Zone Slot 8
+ATCA_STATUS iotc_ecc608_write_all_data(void);
+
+#endif // IOTC_ECC608_H
